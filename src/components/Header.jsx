e99409bbee879eddc03bd32e394cc418e6ada44b
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2022-08-23 21:37:15 -0300

    FEITO EXERCICIO 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -22,7 +23,29 @@ class Header extends Component {
     return (
       <div data-testid="header-component">
         { name ? (
-          <p>Favoritos</p>
+          <nav>
+            <Link
+              to="/search"
+              data-testid="link-to-search"
+            >
+              Search
+            </Link>
+
+            <Link
+              to="/favorites"
+              data-testid="link-to-favorites"
+            >
+              Favorites
+            </Link>
+
+            <Link
+              to="/profile"
+              data-testid="link-to-profile"
+            >
+              Profile
+            </Link>
+          </nav>
+
         ) : (
           <Loading />
         )}
