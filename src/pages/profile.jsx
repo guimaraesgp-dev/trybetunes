72fcb2acc9f591a7b4fb23cee32ccfe97551ea6b
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2022-08-24 15:39:47 -0300

    FEITO EXERCICIO 13

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class profile extends Component {
+  state = {
+    infosUser: {},
+    loading: false,
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const user = await getUser();
+    this.setState({ loading: false, infosUser: user });
+  }
+
   render() {
+    const { loading, infosUser } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
-        PROFILE
+        {loading ? <Loading /> : (
+          <>
+            <img
+              src={ infosUser.image }
+              alt={ infosUser.name }
+              data-testid="profile-image"
+            />
+            <p>Nome</p>
+            <p>{ infosUser.name }</p>
+            <p>Email</p>
+            <p>{ infosUser.email}</p>
+            <p>Descrição</p>
+            <p>{ infosUser.description}</p>
+            <Link to="/profile/edit"> Editar perfil </Link>
+          </>
+        )}
       </div>
     );
   }
